955ab972261ee24194fd9149486eeda31d3a9f70
kaledqal <kalenshi@gmail.com>
2019-10-12 07:25:32 -0300

    Changes to be committed: 	new file:   awa/awa/__init__.py 	new file:   awa/awa/settings.py 	new file:   awa/awa/urls.py 	new file:   awa/awa/wsgi.py 	new file:   awa/manage.py 	new file:   weatherEnv/bin/activate 	new file:   weatherEnv/bin/activate.csh 	new file:   weatherEnv/bin/activate.fish 	new file:   weatherEnv/bin/django-admin 	new file:   weatherEnv/bin/django-admin.py 	new file:   weatherEnv/bin/easy_install 	new file:   weatherEnv/bin/easy_install-3.7 	new file:   weatherEnv/bin/pip 	new file:   weatherEnv/bin/pip3 	new file:   weatherEnv/bin/pip3.7 	new file:   weatherEnv/bin/python 	new file:   weatherEnv/bin/python3 	new file:   weatherEnv/bin/sqlformat 	new file:   weatherEnv/pyvenv.cfg

---
+++ weatherEnv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/kalenshikatebe/Documents/RESTAPI/advanced-weather-api/weatherEnv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
